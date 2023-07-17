21f720182fafc8595a3c3d3016590a6fcfa0aa64
Stefano Novelli <murdercode@gmail.com>
2023-07-17 11:56:48 +0200

    First button stack

---
+++ src/distico.js
@@ -0,0 +1,64 @@
+tinymce.PluginManager.add('distico', function(editor, url) {
+  var openDialog = function () {
+    return editor.windowManager.open({
+      title: 'Distico',
+      body: {
+        type: 'panel',
+        items: [
+          {
+            type: 'textarea',
+            name: 'distico',
+            label: 'Add distico content in your post'
+          }
+        ]
+      },
+      buttons: [
+        {
+          type: 'cancel',
+          text: 'Close'
+        },
+        {
+          type: 'submit',
+          text: 'Save',
+          primary: true
+        }
+      ],
+      onSubmit: function (api) {
+        var data = api.getData();
+        /* Insert content when the window form is submitted */
+        editor.insertContent('[distico]' + data.distico + '[/distico]');
+        api.close();
+      }
+    });
+  };
+
+  /* Add a distico icon */
+  editor.ui.registry.addIcon('distico', '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M432 64H208c-8.8 0-16 7.2-16 16V96H128V80c0-44.2 35.8-80 80-80H432c44.2 0 80 35.8 80 80V304c0 44.2-35.8 80-80 80H416V320h16c8.8 0 16-7.2 16-16V80c0-8.8-7.2-16-16-16zM0 192c0-35.3 28.7-64 64-64H320c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V192zm64 32c0 17.7 14.3 32 32 32H288c17.7 0 32-14.3 32-32s-14.3-32-32-32H96c-17.7 0-32 14.3-32 32z"/></svg>')
+
+  /* Add a button that opens a window */
+  editor.ui.registry.addButton('distico', {
+    icon: 'distico',
+    tooltip: 'Add Distico',
+    onAction: function () {
+      /* Open window */
+      openDialog();
+    }
+  });
+  /* Adds a menu item, which can then be included in any menu via the menu/menubar configuration */
+  editor.ui.registry.addMenuItem('distico', {
+    text: 'Distico',
+    onAction: function() {
+      /* Open window */
+      openDialog();
+    }
+  });
+  /* Return the metadata for the help plugin */
+  return {
+    getMetadata: function () {
+      return  {
+        name: 'Distico Shortcode',
+        url: 'https://github.com/The-3Labs-Team/tinymce-laravel-shortcode-plus'
+      };
+    }
+  };
+});
