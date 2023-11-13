88ef9515a9743b43e7cb34cbe1dec681ed2f4d13
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-11-13 09:57:49 +0100

    Fix Widgetbay code on shortcodeList.js

--- src/shortcodeList.js
+++ src/shortcodeList.js
@@ -24,7 +24,7 @@ tinymce.PluginManager.add('shortcodeList', function (editor, url) {
     },
     {
       title: 'Widgetbay',
-      code: '[widgetbay id="optional" link="optional"',
+      code: '[widgetbay id="optional" link="optional"]',
       description: 'Insert a widgetbay iframe. You can use the id or the link, but not both'
     },
     {
