05ee74d259bb284a98f7ae9758f64c2b49016532
Claudio-Emmolo <113107618+Claudio-Emmolo@users.noreply.github.com>
2024-01-26 17:39:01 +0100

    Refactor survey (from cards to table)

--- src/survey.js
+++ src/survey.js
@@ -9,14 +9,21 @@ tinymce.PluginManager.add('survey', function (editor, url) {
     </section>
 
 
-    <form method="GET" id="survey-data" style="color: black; margin: 20px 10px;">
-        <div id="last-survey-container" style="display: grid; gap: 20px; grid-template-columns: repeat(3, 1fr); grid-template-rows: repeat(3, 1fr); height: 600px; overflow: auto;">
-        
-        </div>
-        
-        <button type="submit" style="background-color: #ff0000; padding: 7px 0; margin: 20px auto; color: white; text-align: center; text-transform: uppercase; width: 100%">Insert</button>
-    </form>
+    <form method="GET" id="survey-data" style="color: black; margin: 20px 10px; max-height: 100vh; overflow: auto">
+        <table style="width: 100%; border-collapse: collapse; margin: 20px 0;">
+          <thead style="border-bottom: 2px solid black;">
+              <tr>
+                  <th style="padding: 10px;">Name</th>
+                  <th style="padding: 10px;">Question</th>
+              </tr>
+          </thead>
+          
+            <tbody id="last-survey-container">
 
+            </tbody>
+
+        </table>
+    </form>
   `
 
   const customStyles = `
@@ -32,6 +39,11 @@ tinymce.PluginManager.add('survey', function (editor, url) {
     .checkboxes:checked + div {
         border: 3px solid #ea1e1e !important;
     }
+        
+    tbody#last-survey-container tr:hover {
+        background-color: #e1e1e1 !important;
+    }
+
   </style>
 `
 
@@ -115,16 +127,17 @@ function printSurvey(surveys) {
   if(surveys.length > 0) {
   surveys.forEach((survey, index) => {
     lastSurveyContainer.innerHTML += `
-    <label for="survey-${survey.id}" style="cursor: pointer">
-    <input type="checkbox" id="survey-${survey.id}" name="survey-${survey.id}" value="${survey.id}" style="display: none" class="checkboxes"></input>
-        <div style="text-align: center; padding: 10px 20px; border: 3px solid black; display: flex; height: 100%;">
-            <div style="margin: auto">
-              <span style="font-size: 1.8rem; font-weight: bold">${survey.name}</span>
-              <br>
-              <span style="font-size: 1.2rem; color: #343434">${survey.question}</span>
-            </div>
-        </div>
-    </label>
+        <tr style="border-bottom: 1px solid #ababab; cursor: pointer; position: relative" >
+            <input type="checkbox" name="survey" id="survey-${survey.id}" value="${survey.id}" class="checkboxes" style="display: none;"
+            onclick="document.querySelector('.tox-dialog__content-js form#survey-data').dispatchEvent(new Event('submit'))">
+            <td style="padding: 10px;">
+                <label for="survey-${survey.id}" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"></label>
+                ${survey.name}
+            </td>
+            <td style="padding: 10px;">
+                ${survey.question}
+            </td>
+        <tr>
   `
   })
   } else {
@@ -159,26 +172,10 @@ function insetData(editor) {
     e.preventDefault()
 
     const formData = new FormData(formSurvey)
-    const ids = getSurveyIds(formData)
+    const id = formData.get('survey')
 
-    const result = `[survey id="${ids}"]`
+    const result = `[survey id="${id}"]`
     editor.insertContent(result)
     tinymce.activeEditor.windowManager.close()
   })
-}
-
-// Get the ids of the selected images
-function getSurveyIds (formData) {
-  let result = ''
-
-  formData.forEach((value, name) => {
-    if (name.startsWith('survey-')) {
-      result += value + ','
-    }
-  })
-
-  if (result.endsWith(',')) {
-    result = result.slice(0, -1)
-  }
-  return result
 }
