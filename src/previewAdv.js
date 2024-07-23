1da521e538866a4ab7c9c6b315c556f7c23f94d0
Claudio-Emmolo <113107618+Claudio-Emmolo@users.noreply.github.com>
2024-07-23 16:46:51 +0200

    Remove previewAdv before save

--- src/previewAdv.js
+++ src/previewAdv.js
@@ -13,14 +13,36 @@ tinymce.PluginManager.add('previewAdv', (editor, url) => {
     }
   })
 
+  editor.on('blur', function () {
+    removeAdvInEditor()
+  })
+
+  editor.on('focus', function () {
+    if (isEnable) {
+      insertAdv()
+    }
+    togglePreviewAdv(isEnable)
+  })
+
+  function togglePreviewAdv (isEnable) {
+    const advDivs = editor.getBody().querySelectorAll('.adv-preview')
+    for (let i = 0; i < advDivs.length; i++) {
+      advDivs[i].style.display = isEnable ? 'block' : 'none'
+    }
+
+    // Update button state
+    setTimeout(() => {
+      const toolbar = editor.getContainer().querySelector('.tox-toolbar-overlord')
+      const btn = toolbar.querySelector('button[aria-label="Enable Preview Adv"]')
+      if (isEnable) {
+        btn.classList.add('tox-tbtn--enabled')
+      } else {
+        btn.classList.remove('tox-tbtn--enabled')
+      }
+    }, 0)
+  }
+
   editor.on('init', function () {
-    const params = editor.getParam('previewAdv') // Get the parameters from config file
-    const thresholds = params.thresholds
-    const blacklist = params.blacklist
-
-    let pCount = 0 // Counter for <p> tags
-    let advCount = 1 // Counter for adv divs
-
     insertAdv()
     togglePreviewAdv(isEnable)
 
@@ -37,87 +59,71 @@ tinymce.PluginManager.add('previewAdv', (editor, url) => {
       clearTimeout(typingTimer)
       typingTimer = setTimeout(addAdvInEditor, typingDebounce)
     })
-
-    // === FUNCTIONS ===
-    function insertAdv () {
-      // REMOVE OLD ADV FOR LOAD NEW
-      removeAdvInEditor()
-
-      const body = editor.getBody()
-      const paragraphs = body.getElementsByTagName('p')
-
-      for (let i = 0; i < paragraphs.length; i++) {
-        if (pCount === thresholds[advCount]) {
-          if (paragraphs[i].nextElementSibling && paragraphs[i].nextElementSibling.classList.contains('adv-preview')) {
-            continue
-          }
-
-          // === BLACKLIST ===
-          const bfBlacklist = blacklist.before.slice(1, -1).split('|')
-          const afBlacklist = blacklist.after.slice(1, -1).split('|')
-
-          bfBlacklist.push('<br', '\\[[^\\]]')
-          afBlacklist.push('<br', '\\[[^\\]]')
-
-          // AFTER BEFORE
-          if (bfBlacklist.some(item => new RegExp(item).test(paragraphs[i].innerHTML))) {
-            continue
-          }
-
-          // AFTER BLACKLIST
-          if (paragraphs[i + 1] && afBlacklist.some(item => new RegExp(item).test(paragraphs[i + 1].innerHTML))) {
-            continue
-          }
-          // === END BLACKLIST ===
-
-          const div = editor.dom.create('div', { class: 'adv-preview', contenteditable: 'false' })
-          div.style.backgroundColor = '#f3f3f3'
-          div.style.color = '#666'
-          div.style.padding = '10px'
-          div.style.border = '1px solid #ccc'
-          div.style.textAlign = 'center'
-          div.style.margin = '10px 0'
-          div.innerHTML = 'Spazio riservato per la pubblicità'
-          editor.dom.insertAfter(div, paragraphs[i])
-          advCount++
-        }
-
-        pCount++
-      }
-    }
-    function addAdvInEditor () {
-      pCount = 0
-      advCount = 1
-
-      // const bookmark = editor.selection.getBookmark(2, true)
-      insertAdv()
-
-      setTimeout(function () {
-        editor.focus()
-        const nextNode = editor.selection.getNode().nextSibling
-
-        if (nextNode) {
-          editor.selection.setCursorLocation(nextNode, -1)
-        }
-      }, 0)
-    }
   })
 
-  function togglePreviewAdv (isEnable) {
-    const advDivs = editor.getBody().querySelectorAll('.adv-preview')
+  // === FUNCTIONS ===
+  function insertAdv () {
+    let pCount = 0
+    let advCount = 1
 
-    for (let i = 0; i < advDivs.length; i++) {
-      advDivs[i].style.display = isEnable ? 'block' : 'none'
+    const params = editor.getParam('previewAdv') // Get the parameters from config file
+    const thresholds = params.thresholds
+    const blacklist = params.blacklist
+
+    // REMOVE OLD ADV FOR LOAD NEW
+    removeAdvInEditor()
+    const body = editor.getBody()
+    const paragraphs = body.getElementsByTagName('p')
+
+    for (let i = 0; i < paragraphs.length; i++) {
+      if (pCount === thresholds[advCount]) {
+        if (paragraphs[i].nextElementSibling && paragraphs[i].nextElementSibling.classList.contains('adv-preview')) {
+          continue
+        }
+
+        // === BLACKLIST ===
+        const bfBlacklist = blacklist.before.slice(1, -1).split('|')
+        const afBlacklist = blacklist.after.slice(1, -1).split('|')
+
+        bfBlacklist.push('<br', '\\[[^\\]]')
+        afBlacklist.push('<br', '\\[[^\\]]')
+
+        // AFTER BEFORE
+        if (bfBlacklist.some(item => new RegExp(item).test(paragraphs[i].innerHTML))) {
+          continue
+        }
+
+        // AFTER BLACKLIST
+        if (paragraphs[i + 1] && afBlacklist.some(item => new RegExp(item).test(paragraphs[i + 1].innerHTML))) {
+          continue
+        }
+        // === END BLACKLIST ===
+
+        const div = editor.dom.create('div', { class: 'adv-preview', contenteditable: 'false' })
+        div.style.backgroundColor = '#f3f3f3'
+        div.style.color = '#666'
+        div.style.padding = '10px'
+        div.style.border = '1px solid #ccc'
+        div.style.textAlign = 'center'
+        div.style.margin = '10px 0'
+        div.innerHTML = 'Spazio riservato per la pubblicità'
+        editor.dom.insertAfter(div, paragraphs[i])
+        advCount++
+      }
+
+      pCount++
     }
+  }
+  function addAdvInEditor () {
+    // const bookmark = editor.selection.getBookmark(2, true)
+    insertAdv()
 
-    // Update button state
-    setTimeout(() => {
-      const toolbar = editor.getContainer().querySelector('.tox-toolbar-overlord')
-      const btn = toolbar.querySelector('button[aria-label="Enable Preview Adv"]')
-      if (isEnable) {
-        btn.classList.add('tox-tbtn--enabled')
-      } else {
-        btn.classList.remove('tox-tbtn--enabled')
+    setTimeout(function () {
+      editor.focus()
+      const nextNode = editor.selection.getNode().nextSibling
+
+      if (nextNode) {
+        editor.selection.setCursorLocation(nextNode, -1)
       }
     }, 0)
   }
